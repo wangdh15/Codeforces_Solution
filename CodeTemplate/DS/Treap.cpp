e79d2e7be24d67e8cf63b63d15fcd0bdfb2a7f6d
wangdh15 <13020023780@163.com>
2020-10-28 14:28:22 +0800

    晚上Treap笔记

--- CodeTemplate/DS/Treap.cpp
+++ CodeTemplate/DS/Treap.cpp
@@ -8,148 +8,169 @@
 #include <iostream>
 using namespace std;
 
-constexpr int N = 1e5 + 5;
-constexpr int INF = 1e8;
+constexpr int N = 1e5 + 10;
 
-struct Node {
-    int l, r;  // 表示左右孩子的编号
-    int key, val; // key表示二叉树的关键字，val表示堆的关键字
-    int cnt, size; // cnt表示当前关键字的个数，size表示以当前节点为根的子树的大小
-} tr[N];
-
-int root, idx;
-
-void pushup(int p) {
-    tr[p].size = tr[tr[p].l].size + tr[tr[p].r].size + tr[p].cnt;
-}
-
-int get_node(int key) {
-    tr[++idx].key = key;
-    tr[idx].val = rand();
-    tr[idx].cnt = tr[idx].size = 1;
-    return idx;
-}
-
-// 右旋操作
-void zig(int &p) {
-
-    int q = tr[p].l;
-    tr[p].l = tr[q].r;
-    tr[q].r = p;
-    pushup(p);
-    pushup(q);
-    p = q;
-}
-
-// 左旋操作
-void zag(int &p) {
-
-    int q = tr[p].r;
-    tr[p].r = tr[q].l;
-    tr[q].l = p;
-    pushup(p);
-    pushup(q);
-    p = q;
-}
-
-// 初始化操作
-void init() {
-    root = get_node(-INF);
-    get_node(INF);
-    tr[1].r = 2;
-    pushup(root);
-    if (tr[1].val < tr[2].val) zag(root); // 如果不满足堆的性质，则进行旋转调整
-}
-
-// 插入一个元素
-void insert(int &p, int key) {
-    if (!p) {
-        p = get_node(key);
+class Treap {
+public:
+    
+    static constexpr int INF = 1e9;
+    
+    explicit Treap(int max_node_cnt):
+    idx(3),
+    data(new Node[max_node_cnt]) {
+        data[1].init(-INF);
+        data[2].init(INF);
+        root = 1;
+        data[1].rc = 2;
+        pushup(root);
+        if (data[1].val < data[2].val) zag(root);
     }
-    else {
-        if (tr[p].key == key) {
-            tr[p].cnt ++;
-        } else if (tr[p].key < key) {
-            insert(tr[p].r, key);
-            if (tr[p].val < tr[tr[p].r].val) zag(p);
-
-        } else {
-            insert(tr[p].l, key);
-            if (tr[p].val < tr[tr[p].l].val) zig(p);
+    ~Treap() {delete []data;}
+    
+    void insert(int key) {_insert(root, key);}
+    void erase(int key) {_erase(root, key);}
+    int get_rank_by_key(int key) {return _get_rank_by_key(root, key) - 1;}
+    int get_key_by_rank(int rank) {return _get_key_by_rank(root, rank + 1);}
+    int get_prev(int key) {return _get_prev(root, key);}
+    int get_next(int key) {return _get_next(root, key);}
+    
+    
+private:
+    
+    class Node {
+    public:
+        int lc, rc; // 记录左孩子， 右孩子还有父节点
+        int key, val;  // 记录当前节点用于平衡树的key，和用于堆的随机生成的val
+        int cnt, size;  // 记录当前节点为根的子树的size和当前key的个数cnt
+        void init(int _key) { // 初始化方法
+            key = _key;
+            cnt = size = 1;
+            lc = rc = 0;
+            val = rand();
         }
+    };
+    
+    Node *data; // 指向申请的节点数组
+    int root;   // 记录根节点的编号
+    int idx; // 记录当前用到了哪个节点
+    
+    
+    // 利用孩子节点的size信息来更新自己的size信息
+    void pushup(int q) {
+        data[q].size = data[data[q].lc].size + data[data[q].rc].size + data[q].cnt;
     }
-    pushup(p);
-}
-
-
-// 删除一个元素
-void erase(int &p, int key) {
-    if (!p) return ;
-    else if (tr[p].key == key) {
-        if (tr[p].cnt > 1) tr[p].cnt --;
-
-        else if (!tr[p].l && !tr[p].r) p = 0;
-
-        else {
-
-            if (!tr[p].r || tr[tr[p].l].val > tr[tr[p].r].val) {
-                zig(p);
-                erase(tr[p].r, key);
-            } else {
-                zag(p);
-                erase(tr[p].l, key);
+    
+    // 右旋算法
+    void zig(int &p) {
+        int q = data[p].lc;
+        data[p].lc = data[q].rc;
+        data[q].rc = p;
+        pushup(p);
+        pushup(q);
+        p = q;
+    }
+    
+    // 左旋算法
+    void zag(int &p) {
+        int q = data[p].rc;
+        data[p].rc = data[q].lc;
+        data[q].lc = p;
+        pushup(p);
+        pushup(q);
+        p = q;
+    }
+    
+    // 插入操作
+    void _insert(int &p, int key) {
+        
+        if (!p) {
+            p = idx ++;
+            data[p].init(key);
+        } else {
+            if (data[p].key == key) data[p].cnt ++;
+            else if (data[p].key > key) {
+                _insert(data[p].lc, key);
+                if (data[p].val < data[data[p].lc].val) zig(p);
+            }
+            else {
+                _insert(data[p].rc, key);
+                if (data[p].val < data[data[p].rc].val) zag(p);
             }
         }
-    } else if (tr[p].key < key) {
-        erase(tr[p].r, key);
-    } else if (tr[p].key > key) {
-        erase(tr[p].l, key);
+        pushup(p);
     }
-    pushup(p);
-}
-
-// 通过值找排名
-int get_rank_by_key(int p, int key) {
-    if (tr[p].key == key) return tr[tr[p].l].size + 1;
-    else if (tr[p].key > key) return get_rank_by_key(tr[p].l, key);
-    else return get_rank_by_key(tr[p].r, key) + tr[tr[p].l].size + tr[p].cnt;
-}
-
-// 通过排名找值
-int get_key_by_rank(int p, int rank) {
-    if (tr[tr[p].l].size >= rank) return get_key_by_rank(tr[p].l, rank);
-    else if (tr[tr[p].l].size + tr[p].cnt < rank) return get_key_by_rank(tr[p].r, rank - tr[tr[p].l].size - tr[p].cnt);
-    else return tr[p].key;
-}
-
-// 得到前驱
-int get_prev(int p, int key) {
-    if (!p) return -INF;
-    if (tr[p].key >= key) return get_prev(tr[p].l, key);
-    else return max(tr[p].key, get_prev(tr[p].r, key));
-}
-
-// 得到后继
-int get_next(int p, int key) {
-    if (!p) return INF;
-    if (tr[p].key <= key) return get_next(tr[p].r, key);
-    else return min(tr[p].key, get_next(tr[p].l, key));
-}
+    
+    // 删除操作
+    void _erase(int &p, int key) {
+        
+        if (!p) return;
+        else if (data[p].key == key) {
+            if (data[p].cnt > 1) data[p].cnt --;
+            else if (!data[p].lc && !data[p].rc) p = 0;
+            else {
+                if (!data[p].rc || data[data[p].lc].val > data[data[p].rc].val) {  // 这个地方注意，利用了rand()的值大于零的性质
+                    zig(p);
+                    _erase(data[p].rc, key);
+                } else {
+                    zag(p);
+                    _erase(data[p].lc, key);
+                }
+            }
+        } else if (data[p].key < key) _erase(data[p].rc, key);
+        else _erase(data[p].lc, key);
+        pushup(p);
+    }
+    
+    // 通过值找排名
+    int _get_rank_by_key(int p, int key) {
+        if (data[p].key == key) return data[data[p].lc].size + 1;
+        else if (data[p].key < key) return _get_rank_by_key(data[p].rc, key) + data[data[p].lc].size + data[p].cnt;
+        else return _get_rank_by_key(data[p].lc, key);
+    }
+    
+    // 通过排名找值
+    int _get_key_by_rank(int p, int rank) {
+        
+        if (data[data[p].lc].size >= rank) return _get_key_by_rank(data[p].lc, rank);
+        else if (data[data[p].lc].size + data[p].cnt < rank) return _get_key_by_rank(data[p].rc, rank - data[data[p].lc].size - data[p].cnt);
+        else return data[p].key;
+        
+    }
+    
+    // 得到前驱
+    int _get_prev(int p, int key) {
+        if (!p) return -INF;
+        if (data[p].key < key) return max(data[p].key, _get_prev(data[p].rc, key));
+        else return _get_prev(data[p].lc, key);
+    }
+    
+    // 得到后继
+    int _get_next(int p, int key) {
+        if (!p) return INF;
+        if (data[p].key > key) return min(data[p].key, _get_next(data[p].lc, key));
+        else return _get_next(data[p].rc, key);
+    }
+};
 
 int main() {
-
+    
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
     int n;
     cin >> n;
-    init();
+    Treap treap(N);
+    
     while (n --) {
         int op, x;
         cin >> op >> x;
-        if (op == 1) insert(root, x);
-        else if (op == 2) erase(root, x);
-        else if (op == 3) cout << get_rank_by_key(root, x) - 1 << '\n';
-        else if (op == 4) cout << get_key_by_rank(root, x + 1) << '\n';
-        else if (op == 5) cout << get_prev(root, x) << '\n';
-        else cout << get_next(root, x) << '\n';
+        if (op == 1) treap.insert(x);
+        else if (op == 2) treap.erase(x);
+        else if (op == 3) cout << treap.get_rank_by_key(x) << '\n';
+        else if (op == 4) cout << treap.get_key_by_rank(x) << '\n';
+        else if (op == 5) cout << treap.get_prev(x) << '\n';
+        else cout <<  treap.get_next(x) << '\n';
     }
     return 0;
 }
