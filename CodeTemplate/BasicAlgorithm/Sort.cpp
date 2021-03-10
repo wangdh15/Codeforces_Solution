e374baf07b7e42f69fba859f661ea35b08a36193
Daohui Wang <13020023780@163.com>
2021-03-10 10:31:49 +0800

    修复缺失do的问题

--- CodeTemplate/BasicAlgorithm/Sort.cpp
+++ CodeTemplate/BasicAlgorithm/Sort.cpp
@@ -14,8 +14,8 @@ void quick_sort(int q[], int l, int r) {
     if (l >= r) return;
     int i = l - 1, j = r + 1, x = q[l + r >> 1];
     while (i < j) {
-        i ++; while (q[i] < x);
-        j --; while (q[j] > x);
+        do i ++; while (q[i] < x);
+        do j --; while (q[j] > x);
         if (i < j) swap(q[i], q[j]);
     }
     quick_sort(q, l, j);
